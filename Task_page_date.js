73d25ab0f5a14fe72d6e26c9d7fcfb30ad1b60b2
tomyslav <tomyivanov06@gmail.com>
2022-04-04 14:58:06 +0300

    files

---
+++ Task_page_date.js
@@ -0,0 +1,32 @@
+let today = new Date();
+let date = today.getFullYear() + "-";
+
+if (today.getMonth()+1 < 10)
+    date += `0${today.getMonth()+1}-`;
+else date += (today.getMonth()+1)+'-';
+
+if (today.getDate() < 10)
+    date += `0${today.getDate()}`;
+else date += (today.getDate());
+
+let a = document.getElementsByClassName("today");
+
+for (let i = 0; i < a.length; i++) {
+  a[i].setAttribute("min", today.toISOString().substr(0, 10));;
+}
+
+function SetTime(){
+    today = new Date()
+    let time;
+    if (date == a.value)
+        time = today.getHours() + ":" + today.getMinutes();
+    else time = "00:00"
+
+    b = document.getElementsByClassName("now")
+
+    for (let k = 0; k < ab.length; k++) {
+        b[k].setAttribute("min", time);;
+    }
+    
+    console.log(time);
+}
---
+++ Task_page_notes.js
@@ -0,0 +1,56 @@
+function addTask() {
+    const form = document.createElement("form");
+    form.setAttribute('class', "input-box");
+    form.setAttribute('action', "Login_page.html");
+    form.setAttribute('method', "post");
+
+    const title = document.createElement("input");
+    title.setAttribute('type', "text");
+    title.setAttribute('class', "title");
+    title.setAttribute('placeholder',"Enter title");
+    title.setAttribute('required', '');
+
+    const today = document.createElement("input");
+    today.setAttribute('type', "date");
+    today.setAttribute('class', "today");
+    today.setAttribute('onchange', "SetTime()");
+    today.setAttribute('required', '');
+
+    const now = document.createElement("input");
+    now.setAttribute('type', "time");
+    now.setAttribute('class', "now");
+    now.setAttribute('value', "23:59");
+    now.setAttribute('min', "7:00:00");
+    now.setAttribute('required', '');
+
+    const discription = document.createElement("input");
+    discription.setAttribute('type', "text");
+    discription.setAttribute('class', "note");
+    discription.setAttribute('required', '');
+    discription.setAttribute('placeholder', "Some discription");
+
+    const submit = document.createElement("input");
+    submit.setAttribute('type', "submit");
+    submit.setAttribute('value', "submit");
+
+    const deleat = document.createElement("button");
+    deleat.setAttribute('type', "button");
+    deleat.setAttribute('class', "deleat-button");
+    deleat.setAttribute('onclick', "removeTask()");
+    deleat.innerHTML = "X";
+
+    form.appendChild(deleat);
+    form.appendChild(title);
+    form.appendChild(today);
+    form.appendChild(now);
+    form.appendChild(discription);
+    form.appendChild(submit);
+
+    document.getElementById("add-task-box").insertBefore(elem, target);
+} 
+
+function removeTask(id)
+{
+    const element = document.getElementById(id);
+    element.remove();
+}
